52f7b538e8740924715d22f53e877cde59d7fa9f
Nitin Jayant <thenitinjayant@gmail.com>
2019-12-28 00:59:23 +0530

    first

---
+++ src/Home.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+import './Home.css'
+
+class Home extends Component
+{
+    render(){
+        return(
+            <div>
+                <h1>Gamolecule</h1>
+            </div>  
+        );
+    }
+        
+}
+
+export default Home;
